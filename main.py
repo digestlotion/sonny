95e2f016087bf108491b4e31881def91f549800b
driftbluestone <driftbluestone@gmail.com>
2026-09-17 16:33:54 -0700

    oops

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def on_message_delete(message: discord.Message):
 
 @bot.event
 async def on_guild_join(guild: discord.Guild):
-    if getattr(bot, "allowed_joins", 0) < 1:
+    if (len(bot.guilds) != 0) and (getattr(bot, "allowed_joins", 0) < 1):
         return await guild.leave()
     setattr(bot, "allowed_joins", getattr(bot, "allowed_joins", 1)-1)
 
